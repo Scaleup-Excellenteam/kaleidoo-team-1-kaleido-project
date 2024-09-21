58f870412f60ab0c19dd99b3d8bbfb35e6dd7f2f
Amir Ammar <amir.ammar.work@gmail.com>
2024-09-21 15:16:39 +0300

    done with the parsing web page

--- server/partition.py
+++ server/partition.py
@@ -13,21 +13,6 @@ from unstructured.partition.epub import partition_epub
 from unstructured.partition.msg import partition_msg
 from unstructured.partition.rst import partition_rst
 
-import os
-from unstructured.partition.pdf import partition_pdf
-from unstructured.partition.doc import partition_doc
-from unstructured.partition.csv import partition_csv
-from unstructured.partition.text import partition_text
-from unstructured.partition.docx import partition_docx
-from unstructured.partition.html import partition_html
-from unstructured.partition.pptx import partition_pptx
-from unstructured.partition.json import partition_json
-from unstructured.partition.xml import partition_xml
-from unstructured.partition.xlsx import partition_xlsx
-from unstructured.partition.epub import partition_epub
-from unstructured.partition.msg import partition_msg
-from unstructured.partition.rst import partition_rst
-
 def partition_file(file_path):
     """Partition a file based on its extension."""
     _, ext = os.path.splitext(file_path)
